12e9dbd59e0983acc96f419995403a2327adf815
rajakushwah <kushwah.raja1@gmail.com>
2024-11-24 19:39:37 +0530

    Implemented a func to upload a video file to database

---
+++ testing.py
@@ -0,0 +1,19 @@
+import requests
+import os
+import json
+
+
+def TestUploadFile():
+    filepath = 'uploads/test.mp4'
+    files = {'video': (os.path.basename(filepath), open(filepath, 'rb'), 'video/mp4')}
+    url = "http://127.0.0.1:5001/upload"
+    headers = {
+        'Authorization': 'abc'
+    }
+    response = requests.post(url, files=files, headers=headers)
+    print(response.text)
+    return response.text
+
+
+x=TestUploadFile()
+
